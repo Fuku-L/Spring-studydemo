427537aeae4ed78c111238189c59608d97b122c5
Fuku-Lee <2971065067@qq.com>
2021-12-09 17:34:43 +0800

    springmvc

--- highlight_springmvc4/src/main/java/com/wisely/config/MyMvcConfig.java
+++ highlight_springmvc4/src/main/java/com/wisely/config/MyMvcConfig.java
@@ -4,13 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.wisely")
-public class MyMvcConfig {
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
 
     // 配置 JSP 的 viewResolver， 用来映射路径和实际页面的位置。
     @Bean
@@ -21,6 +23,14 @@ public class MyMvcConfig {
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
         return viewResolver;
+    }
+
+
+    // Srping MVC 的定制配置需要 MyMvcConfig 配置类继承 WebMvcConfigurerAdapter.
+    // 静态资源映射
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
     }
 
 }
