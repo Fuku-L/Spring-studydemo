0d6f941d3227ceb4366a43abc7bc2049c1a4bed6
Fuku-Lee <2971065067@qq.com>
2021-12-15 17:31:44 +0800

    springboot_jpa使用

---
+++ highlight_spring_jpa/springboot_jpa/src/main/java/org/example/springboot_jpa/JPAwebApplication.java
@@ -0,0 +1,11 @@
+package org.example.springboot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPAwebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JPAwebApplication.class,args);
+    }
+}
---
+++ highlight_spring_jpa/springboot_jpa/src/main/java/org/example/springboot_jpa/dao/PersonRepository.java
@@ -0,0 +1,20 @@
+package org.example.springboot_jpa.dao;
+
+import org.example.springboot_jpa.domain.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    List<Person> findByAddress(String name);
+
+    Person findByNameAndAddress(String name,String address);
+    
+    @Query("select p from Person p where p.name= :name and p.address= :address")
+    Person withNameAndAddressQuery(@Param("name")String name, @Param("address")String address);
+
+    // 使用 @NamedQuery 查询： 在实体类Person上添加了 @NamedQuery 的定义
+    List<Person> withNameAndAddressNamedQuery(String name, String address);
+}
