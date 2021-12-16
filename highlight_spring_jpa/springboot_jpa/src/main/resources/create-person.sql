de39029c0dd5980912030c319aa840a2c9ed94ea
Fuku-Lee <2971065067@qq.com>
2021-12-16 15:06:50 +0800

    springboot_jpa使用-完善项目注释和结构

---
+++ highlight_spring_jpa/springboot_jpa/src/main/resources/create-person.sql
@@ -0,0 +1,30 @@
+-- 本 sql 脚本是用户 boot 下的执行建表语句和数据插入语句
+-- 建表语句
+-- 存储过程：如果存在表，先删除表再创建，如果存在序列，先删除序列。
+declare
+    num   number;
+    tname varchar2(50) :='person';
+    seqname varchar2(50) :='hibernate_sequence';
+begin
+    select count(1) into num from user_tables where table_name = upper(tname) ;
+    if num > 0 then
+        execute immediate 'drop table '|| tname ;
+    end if;
+    num := 0;
+    select  count(1) into num  from  user_sequences  where  sequence_name= upper(seqname) ;
+    if num > 0 then
+        execute immediate 'drop sequence '|| seqname ;
+    end if;
+end;
+
+create sequence hibernate_sequence
+    minvalue 0;
+
+-- drop sequence seq_personid;
+
+insert into person(id,name,age,address) values(hibernate_sequence.nextval, 'aa', 10, '合肥');
+insert into person(id,name,age,address) values(hibernate_sequence.nextval, 'yy', 11, '北京');
+insert into person(id,name,age,address) values(hibernate_sequence.nextval, 'bb', 12, '上海');
+insert into person(id,name,age,address) values(hibernate_sequence.nextval, 'cc', 13, '南京');
+insert into person(id,name,age,address) values(hibernate_sequence.nextval, 'dd', 14, '武汉');
+insert into person(id,name,age,address) values(hibernate_sequence.nextval, 'ee', 15, '广州');
---
+++ highlight_spring_jpa/springboot_jpa/target/classes/create-dbaUser.sql
@@ -0,0 +1,26 @@
+-- 本 sql 脚本用户创建 boot 用户，并给用户授权。
+-- 创建 sys(系统管理员，最高权限) system(本地管理员，次高权限)，scott(普通用户)
+
+-- 登录
+-- sqlplus / as sysdba; // 登录到 sys
+-- sqlplus sys as sysdba; //登录到sys
+-- sqlplus scott/tiger; //登录到普通用户scott
+
+-- 管理用户
+create user boot identified by boot; -- 在管理员账户下，创建用户 boot
+alter user boot identified by boot; -- 修改密码
+
+-- 给新建用户授权
+-- 查看当前用户权限
+select * from USER_SYS_PRIVS;
+
+--
+grant select,resource,dba to boot;
+-- 授权
+grant connect to boot;
+grant Resource to boot;
+grant GLOBAL QUERY REWRITE to boot;
+grant CREATE TABLE to boot;
+grant UNLIMITED TABLESPACE to boot;
+grant select any table to boot;
+
